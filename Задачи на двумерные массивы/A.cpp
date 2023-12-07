f1488b12964b8513e69fe2f1de19073247897734
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2023-12-07 20:42:54 +0600

    Update A.cpp

--- Задачи на двумерные массивы/A.cpp
+++ Задачи на двумерные массивы/A.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n, 0));
+
+    for (int i = 0; i < n; ++i) {
+        matrix[i][n - i - 1] = 1;
+        for (int j = 0; j < n - i - 1; ++j) {
+            matrix[i][j] = 0;
+        }
+        for (int j = n - i; j < n; ++j) {
+            matrix[i][j] = 2;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+         cout << endl;
+    }
+
+    return 0;
+}
 
