0879293c19f47050f8ea3c56ec7382f0f7628d2e
lrmrvv <145489385+lrmrvv@users.noreply.github.com>
2023-12-07 23:04:05 +0600

    Create C.cpp

---
+++ Функции/C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+int main() {
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    double result = distance(x1, y1, x2, y2);
+    cout << result << endl;
+
+    return 0;
+}
